05af8badf6c2ca45b5753ce905f1366f6b9ef138
milk <gongzhenyu40@qq.com>
2021-08-02 03:00:20 +0800

    finish Menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,35 @@
-import { Affix, Layout } from 'antd';
+import { Affix, Col, Layout, Menu, Row } from 'antd';
 import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import { nav } from '../router';
 
 export default function Header() {
+  const { pathname } = useLocation();
   return (
     <Affix
       offsetTop={0}
     >
-      <Layout.Header>
-        <div className="wrap"></div>
+      <Layout.Header id="header">
+        <div className="wrap">
+          <Row>
+            <Col xs={6} sm={5} md={2}>
+              <h1 className="logo">
+                <Link to="/">logo</Link>
+              </h1>
+            </Col>
+            <Col xs={18} sm={19} md={22}>
+              <Menu
+                mode="horizontal"
+                theme="dark"
+                defaultSelectedKeys={[pathname]}
+              >
+                {nav.map(item => <Menu.Item key={item.to}>
+                  <Link to={item.to}>{item.txt}</Link>
+                </Menu.Item>)}
+              </Menu>
+            </Col>
+          </Row>
+        </div>
       </Layout.Header>
     </Affix>
   )
--- src/router/index.js
+++ src/router/index.js
@@ -44,5 +44,16 @@ const route = [{
   }
 }];
 
-export { route };
+const nav = [{
+  to: '/',
+  txt: '首页'
+}, {
+  to: '/getStart',
+  txt: '新手入门'
+}, {
+  to: '/about',
+  txt: '关于我们'
+}]
+
+export { route, nav };
 
